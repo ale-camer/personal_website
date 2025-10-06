3c115df8be8c524f179aacbf42d7c1bad15d3f6e
Alejandro Camerlengo <ale_camer@hotmail.com>
2025-10-06 18:55:18 +0200

    whatsapp data process refactored

--- modules/refactoring/whatsapp.py
+++ modules/refactoring/whatsapp.py
@@ -1,59 +1,9 @@
-import pandas as pd
-
-from dataclasses import dataclass
 import re
 from tqdm import tqdm
-from datetime import datetime as dt
+from datetime import datetime
+from collections import Counter
 
 from utils import read_txt
-
-@dataclass
-class WhatsAppConfig:
-    df: pd.DataFrame
-    content: pd.DataFrame
-    language: str
-
-class WhatsAppParser:
-    CHUNK_SIZE = 1_000_000
-    RAW_DATA = 'RAW_DATA'
-    DATE = 'DATE'
-    HOUR = 'HOUR'
-    ISSUER = 'ISSUER'
-    MESSAGE = 'MESSAGE'
-    COLS_TO_GROUP = ['HOUR', 'dow', 'dom', 'month']
-    COLUMNS = [DATE, HOUR, ISSUER, MESSAGE, 'dow', 'dom', 'month', 'len_message']
-
-    def __init__(self, file):
-        self.file = file
-
-    def group(self, df: pd.DataFrame) -> pd.DataFrame: # 2
-        by_issuer = self._group_and_count(df, [self.ISSUER] + self.COLS_TO_GROUP)
-        general = self._group_and_count(df, self.COLS_TO_GROUP).assign(ISSUER='GENERAL')
-        return pd.concat([by_issuer, general], ignore_index=True)
-
-    def _group_and_count(self, df: pd.DataFrame, group_columns: list) -> pd.DataFrame:
-        return df.groupby(group_columns)[self.MESSAGE].count().reset_index()
-
-class WhatsAppModule:
-    ISSUER = 'ISSUER'
-
-    def __init__(self):
-        self.current_data = None
-
-    def parse_chat(self, file, language: str) -> WhatsAppConfig:
-        parser = WhatsAppParser(file)
-        content = parser.parse()
-        df = parser.group(content)
-        self.current_data = WhatsAppConfig(df=df, content=content, language=language)
-        return self.current_data
-
-#%%
-
-# CHUNK_SIZE = 1_000
-# COLS_TO_GROUP = ['HOUR', 'dow', 'dom', 'month']
-# ['DATE', 'HOUR', 'ISSUER', 'MESSAGE', 'dow', 'dom', 'month', 'len_message']
-
-#%%
 
 def clean_message(data: str) -> iter:
 
@@ -75,7 +25,7 @@ def clean_message(data: str) -> iter:
     def parse_line(line: str) -> tuple:
         generic_match = re.compile(SPLIT_STR).match(line)
         date, time, issuer, msg = generic_match.groups()
-        date = dt.strptime(date, "%d/%m/%Y")
+        date = datetime.strptime(date, "%d/%m/%Y")
         return (
             date, int(time[:2]), issuer.strip(), msg.strip(),
             date.weekday(), date.day, date.month, msg.count(" ")+1
@@ -85,7 +35,15 @@ def clean_message(data: str) -> iter:
 
 def parse_messages(data):
     MESSAGE = "Cleaning messages"
-    return [msg for msg in tqdm(clean_message(data), desc=MESSAGE+"with list comp")]
+    return [msg for msg in tqdm(clean_message(data), desc=MESSAGE)]
 
-data = read_txt("whatsapp_chat.txt").splitlines() * 500
+def groupby_dict(data):
+    return Counter(
+        f"{prefix}{'_'.join(str(row[i]) for i in idxs)}"
+        for row in tqdm(data, desc="Grouping data")
+        for prefix, idxs in [("", [2,1,4,5,6]), ("GENERAL_", [1,4,5,6])]
+    )
+
+data = read_txt("whatsapp_chat.txt").splitlines()
 cleaned_data = parse_messages(data)
+grouped_data = groupby_dict(cleaned_data)
